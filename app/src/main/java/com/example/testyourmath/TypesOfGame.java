e04c069c482d6346e419fe60f4a8750efa2f3e5e
snehal <snehal.shirgire@gmail.com>
2020-09-22 21:27:15 +0530

    add logo.png add code for back button.

--- app/src/main/java/com/example/testyourmath/TypesOfGame.java
+++ app/src/main/java/com/example/testyourmath/TypesOfGame.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 
 public class TypesOfGame extends AppCompatActivity {
 
-    Button btn_classic , btn_timer;
+    Button btn_classic , btn_timer,btn_back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +19,7 @@ public class TypesOfGame extends AppCompatActivity {
 
         btn_classic = findViewById(R.id.btn_classic);
         btn_timer = findViewById(R.id.btn_timer);
+        btn_back=findViewById(R.id.btn_back);
 
         btn_timer.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,6 +33,13 @@ public class TypesOfGame extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(TypesOfGame.this , ClassicGame.class);
+                startActivity(intent);
+            }
+        });
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TypesOfGame.this ,MainActivity.class);
                 startActivity(intent);
             }
         });
